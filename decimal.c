7ccbcfe948443f551b6d4ae3cda354e40a8bea2d
Ricardo Fernandes Ribeiro <ricardo.fernandes.ribeiro.06@gmail.com>
2025-05-14 18:21:58 -0300

    Add files via upload

---
+++ decimal.c
@@ -0,0 +1,42 @@
+/*
+Autor: Ricardo Fernandes Ribeiro
+Curso: Sistemas de Informação - 1 Período
+Nome do projeto: Conversor de decimal
+*/
+
+#include<stdio.h>
+
+void converteBinario (int dec)
+{
+
+    int resto[8], i = 0;
+
+    while (dec >= 1)
+    {
+
+        resto[i++] = dec % 2;
+        dec = dec / 2;
+
+    }
+    while (i > 0)
+    {
+
+        printf("%d", resto[--i]);
+
+    }
+}
+
+
+int main ()
+{
+
+   int decimal;
+
+   printf("Digite um número decimal:");
+   scanf("%d", &decimal);
+
+   converteBinario(decimal);
+
+   return 0;
+}
+
